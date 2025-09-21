a28c89787fe012d708b6f20cee0f96b9a731388d
Marcus Dillavou <line72@line72.net>
2025-09-21 09:40:09 -0500

    Added migration to add new properties to stations

--- alembic/versions/976eefd09ec2_add_options_to_the_station.py
+++ alembic/versions/976eefd09ec2_add_options_to_the_station.py
@@ -19,8 +19,20 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    pass
+    # Add several new options to the stations table
+    
+    op.add_column('stations', sa.Column('replay_song_cooldown', sa.Integer(), nullable=True, default=50))
+    op.add_column('stations', sa.Column('replay_artist_downrank', sa.Float(), nullable=True, default=0.995))
+    op.add_column('stations', sa.Column('ignore_live', sa.Boolean(), nullable=True, default=False))
+    
+    # Set default values for existing rows
+    op.execute("UPDATE stations SET replay_song_cooldown = 50 WHERE replay_song_cooldown IS NULL")
+    op.execute("UPDATE stations SET replay_artist_downrank = 0.995 WHERE replay_artist_downrank IS NULL")
+    op.execute("UPDATE stations SET ignore_live = 0 WHERE ignore_live IS NULL")
 
 
 def downgrade() -> None:
-    pass
+    op.drop_column('stations', 'ignore_live')
+    op.drop_column('stations', 'replay_artist_downrank')
+    op.drop_column('stations', 'replay_song_cooldown')
+             
