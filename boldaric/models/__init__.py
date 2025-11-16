3932da47f0b385552cf6685ecbf7e2f541b1156c
Marcus Dillavou <line72@line72.net>
2025-11-16 16:27:49 -0600

    Removed embedding history completely

--- boldaric/models/__init__.py
+++ boldaric/models/__init__.py
@@ -5,7 +5,6 @@ Base = declarative_base()
 from .user import User
 from .station import Station
 from .track_history import TrackHistory
-from .embedding_history import EmbeddingHistory
 from .track import Track
 from .genre import Genre
 from .track_genre import TrackGenre
