08e5cf894e89d8934b21f83ca8ddac06cf1dec82
Marcus Dillavou <line72@line72.net>
2025-11-30 15:58:42 -0600

    Fixed some typos in the server

--- boldaric/server.py
+++ boldaric/server.py
@@ -84,13 +84,13 @@ def get_next_songs(
     ignore_list.extend([(x.artist, x.title) for x in thumbs_downed])
     # ignore last X played
     replay_song_cooldown = station_options.replay_song_cooldown
-    ignore_list.extend([(x.artist, x.title) for x in played[-replay_song_cooldown:]])
+    ignore_list.extend([(x.track.artist, x.track.track) for x in played[-replay_song_cooldown:]])
     logger.debug(f"ignoring {ignore_list}")
 
     tracks = db.query_similar(new_embeddings, n_results=45, ignore_songs=ignore_list)
 
     # resort these, and slightly downvote recent artists
-    recent_artists = [x.artist for x in played[-15:]]
+    recent_artists = [x.track.artist for x in played[-15:]]
 
     def update_similarity(t):
         replay_artist_downrank = station_options.replay_artist_downrank
