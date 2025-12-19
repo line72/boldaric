bbab97d30297ca7728a62998136442519c2096a5
Marcus Dillavou <line72@line72.net>
2025-12-19 13:18:07 -0500

    Updated some of the scripts for testing

--- scripts/compare-two-songs.py
+++ scripts/compare-two-songs.py
@@ -86,25 +86,41 @@ def make_color_cont(v, min_v, max_v):
     forty = max_v * 0.4
 
     if v > forty:
-        return f'[bold green]{v}'
+        return f'[bold green]{v:.6f}'
     elif v < 0:
-        return f'[bold red]{v}'
+        return f'[bold red]{v:.6f}'
     else:
-        return f'{v}'
+        return f'{v:.6f}'
 
 def make_color_diff(v, min_v, max_v):
     epsilon1 = 1e-3
     epsilon2 = 1e-1
 
     if v <= epsilon1:
-        return f'[bold green]{v}'
+        return f'[bold green]{v:.6f}'
     elif v > epsilon2:
-        return f'[bold red]{v}'
+        return f'[bold red]{v:.6f}'
     else:
-        return f'{v}'
+        return f'{v:.6f}'
     
 def compute_cosine_similarity_explanation(name, query_embedding, result_embedding):
     results = boldaric.utils.compute_cosine_similarity_explanation(
+        query_embedding, result_embedding)
+
+    results['name'] = name
+
+    return results
+
+def compute_l2_similarity_explanation(name, query_embedding, result_embedding):
+    results = boldaric.utils.compute_l2_similarity_explanation(
+        query_embedding, result_embedding)
+
+    results['name'] = name
+
+    return results
+
+def compute_ip_similarity_explanation(name, query_embedding, result_embedding):
+    results = boldaric.utils.compute_ip_similarity_explanation(
         query_embedding, result_embedding)
 
     results['name'] = name
@@ -115,24 +131,38 @@ def main(db, song1_id, song2_id):
     song1 = db.get_track_by_subsonic_id(song1_id)
     song2 = db.get_track_by_subsonic_id(song2_id)
 
-    results1 = compute_cosine_similarity_explanation('Old',
-        boldaric.feature_helper.track_to_embeddings_old_normalization(song1),
-        boldaric.feature_helper.track_to_embeddings_old_normalization(song2)
+    MODULE = boldaric.feature_helper.GenreFeatureHelper
+    
+    cosine = compute_cosine_similarity_explanation('Cosine',
+        MODULE.track_to_embeddings(song1),
+        MODULE.track_to_embeddings(song2)
     )
-    results2 = compute_cosine_similarity_explanation('Default',
-        boldaric.feature_helper.track_to_embeddings_default_normalization(song1),
-        boldaric.feature_helper.track_to_embeddings_default_normalization(song2)
+    
+    l2 = compute_l2_similarity_explanation('L2',
+        MODULE.track_to_embeddings(song1),
+        MODULE.track_to_embeddings(song2)
     )
-    results3 = compute_cosine_similarity_explanation('Mood',
-        boldaric.feature_helper.track_to_embeddings_mood(song1),
-        boldaric.feature_helper.track_to_embeddings_mood(song2)
+    
+    ip = compute_ip_similarity_explanation('IP',
+        MODULE.track_to_embeddings(song1),
+        MODULE.track_to_embeddings(song2)
     )
-    results4 = compute_cosine_similarity_explanation('Genre',
-        boldaric.feature_helper.track_to_embeddings_genre(song1),
-        boldaric.feature_helper.track_to_embeddings_genre(song2)
-    )
+    
+    # results2 = compute_cosine_similarity_explanation('Default',
+    #     boldaric.feature_helper.DefaultFeatureHelper.track_to_embeddings(song1),
+    #     boldaric.feature_helper.DefaultFeatureHelper.track_to_embeddings(song2)
+    # )
+    # results3 = compute_cosine_similarity_explanation('Mood',
+    #     boldaric.feature_helper.MoodFeatureHelper.track_to_embeddings(song1),
+    #     boldaric.feature_helper.MoodFeatureHelper.track_to_embeddings(song2)
+    # )
+    # results4 = compute_cosine_similarity_explanation('Genre',
+    #     boldaric.feature_helper.GenreFeatureHelper.track_to_embeddings(song1),
+    #     boldaric.feature_helper.GenreFeatureHelper.track_to_embeddings(song2)
+    # )
 
-    print_results(song1, song2, [results1, results2, results3, results4])
+    #print_results(song1, song2, [results1, results2, results3, results4])
+    print_results(song1, song2, [cosine, l2, ip])
     
 
 if __name__ == '__main__':
--- scripts/test-similarities.py
+++ scripts/test-similarities.py
@@ -6,6 +6,9 @@
 import os
 import argparse
 
+import rich.table
+import rich.console
+
 import boldaric
 from boldaric.models.track import Track
 
@@ -31,6 +34,21 @@ checks = [
         ("Darkthrone","Transilvanian Hunger","Slottet i det fjerne"),
         False, True
     ),
+    (
+        ("Smashing Pumpkins","Siamese Dream","Today"),
+        ("Smashing Pumpkins","Siamese Dream","Disarm"),
+        True, True
+    ),
+    (
+        ("The Smashing Pumpkins","Mellon Collie and the Infinite Sadness","Bullet With Butterfly Wings"),
+        ("Smashing Pumpkins","Siamese Dream","Today"),
+        True, True
+    ),
+    (
+        ("Nirvana","In Utero","All Apologies"),
+        ("Smashing Pumpkins","Siamese Dream","Today"),
+        True, True
+    )
 ]
 
 def check_score(outcome, score, needed_score):
@@ -41,7 +59,17 @@ def check_score(outcome, score, needed_score):
     else:
         return '❌'
 
-def main(db):
+def main(db, score):
+    table = rich.table.Table()
+    table.add_column('Song 1')
+    table.add_column('Song 2')
+    table.add_column('Similar Mood?')
+    table.add_column('Pass?')
+    table.add_column('Score', max_width=7)
+    table.add_column('Similar Genre?')
+    table.add_column('Pass?')
+    table.add_column('Score', max_width=7)
+    
     with db.Session() as session:
         for a1, a2, mood, genre in checks:
             song1 = session.query(Track).filter(Track.artist == a1[0],
@@ -52,21 +80,29 @@ def main(db):
                                                 Track.title == a2[2]).first()
 
             results1 = boldaric.utils.compute_cosine_similarity_explanation(
-                boldaric.feature_helper.track_to_embeddings_mood(song1),
-                boldaric.feature_helper.track_to_embeddings_mood(song2)
+                boldaric.feature_helper.MoodFeatureHelper.track_to_embeddings(song1),
+                boldaric.feature_helper.MoodFeatureHelper.track_to_embeddings(song2)
             )
             results2 = boldaric.utils.compute_cosine_similarity_explanation(
-                boldaric.feature_helper.track_to_embeddings_genre(song1),
-                boldaric.feature_helper.track_to_embeddings_genre(song2)
+                boldaric.feature_helper.GenreFeatureHelper.track_to_embeddings(song1),
+                boldaric.feature_helper.GenreFeatureHelper.track_to_embeddings(song2)
             )
 
-            NEEDED_SCORE=0.75
+            NEEDED_SCORE=score
             
             mood_results = check_score(mood, results1['similarity_score'], NEEDED_SCORE)
             genre_results = check_score(genre, results2['similarity_score'], NEEDED_SCORE)
 
-            print(f'{song1.artist} - {song1.title} vs {song2.artist} - {song2.title}: {mood_results}|{genre_results}')
+            table.add_row(
+                f'{song1.artist} - {song1.title}',
+                f'{song2.artist} - {song2.title}',
+                str(mood), mood_results, str(results1["similarity_score"]),
+                str(genre), genre_results, str(results2["similarity_score"])
+            )
+            #print(f'{song1.artist} - {song1.title} vs {song2.artist} - {song2.title}: {mood_results}|{genre_results}')
 
+    console = rich.console.Console()
+    console.print(table)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Similarity Analyzer")
@@ -77,11 +113,18 @@ if __name__ == '__main__':
         dest="db_path",
         help="Path to the station database",
     )
+    parser.add_argument(
+        "-s",
+        "--score",
+        type=float,
+        default=0.9,
+        help="Minimum score to be considered similar"
+    )
     args = parser.parse_args()
 
     db_name = os.path.join(args.db_path, "stations.db")
     stationdb = boldaric.StationDB(db_name)
     
-    main(stationdb)
+    main(stationdb, args.score)
     
 
